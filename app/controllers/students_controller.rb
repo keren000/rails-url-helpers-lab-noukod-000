6d0f92da7a9504151413f374762d2c1b3b73dfca
Keren-Happuc Fleurimond <keren@noukod.com>
2019-01-22 14:36:04 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,7 +4,7 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
-  
+
   def show
   end
 
@@ -12,7 +12,7 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
     @student.active = !@student.active
     @student.save
-    redirect_to student_path(@student) 
+    redirect_to student_path(@student)
   end
 
   private
