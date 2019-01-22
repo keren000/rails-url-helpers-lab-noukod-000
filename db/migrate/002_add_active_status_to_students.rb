5dfc58e2f2800ef5c99de13b6ad8c19df87438ff
Keren-Happuc Fleurimond <keren@noukod.com>
2019-01-22 14:32:46 +0000

    Automatically backed up by Learn

--- db/migrate/002_add_active_status_to_students.rb
+++ db/migrate/002_add_active_status_to_students.rb
@@ -1,5 +1,5 @@
-class AddActiveColumnToStudents < ActiveRecord::Migration
+class AddActiveStatusToStudents < ActiveRecord::Migration
   def change
-    add_column :students, :active, :boolean, default: false
+    add_column :students, :active, :boolean, :default => 1
   end
 end
